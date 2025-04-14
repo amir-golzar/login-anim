db099377b83d341e669d6166fc1137a383547e12
amir-golzar <goliamir1384@gmail.com>
2025-04-14 14:15:03 +0000

    salty

--- backend/back.js
+++ backend/back.js
@@ -1,23 +1,37 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const gt = require('./jwt/GT');
-const express = require('express');
-const cors = require('cors');
-const body = require('body-parser');
+const gt = require("./jwt/GT");
+const express = require("express");
+const cors = require("cors");
+const body = require("body-parser");
 
-const env = require('dotenv');
-env.config()
+const env = require("dotenv");
+env.config();
 
-const app=express()
+const app = express();
 
-app.use(cors({origin:"*"}))
-app.use(body.json())
+app.use(cors({ origin: "*" }));
+app.use(body.json());
 
+const mongooseURL = mongoose.connect("mongodb://127.0.0.1:27017/local");
 
+const mSchema = mongoose.Schema({
+  userName: { type: "String", required: true },
+  email: { type: "String", required: true },
+  password: { type: "String", required: true },
+});
 
-app.post("/soinin",(req,res) => {
-    res.json("kir")
+mSchema.pre("save",async (e) => {
+    const salt=await bcrypt.genSalt(12)
+    this.password=await bcrypt.hash(this.password,salt)
 })
 
-app.listen(process.env.PORT)
+
+const user=mongoose.model("soinin",mSchema)
+
+app.post("/soinin", (req, res) => {
+  res.json("kir");
+});
+
+app.listen(process.env.PORT);
