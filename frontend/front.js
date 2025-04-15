6a26f94aaf1e1364d17c7de81ec526c3e2b9a777
amir-golzar <goliamir1384@gmail.com>
2025-04-15 06:54:53 +0000

    FETCH

--- frontend/front.js
+++ frontend/front.js
@@ -1,3 +1,15 @@
-const form=document.getElementById("dale")
+const form = document.getElementById("dale");
 
-form.addEventListener("submit")
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const data = { name, email, password };
+
+  const response = await fetch("http://127.0.01:5500/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+});
