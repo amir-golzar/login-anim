a501d396bef2b4ab9bdaef72341d338713e59c75
amir-golzar <goliamir1384@gmail.com>
2025-04-19 12:45:47 +0000

    k

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -2,8 +2,8 @@ const form = document.getElementById("dale");
 
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
@@ -24,19 +24,21 @@ form.addEventListener("submit", async (e) => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
-  
-  const {message,token} = await response.json();
+
+  const { message, token } = await response.json();
   alert(message);
   console.log(token);
-  setCookie("tokenjwt",token,30);
-
+  setCookie("tokenjwt", token, 30);
 
   if ((data.name == "", data.email == "", data.password == "")) {
     alert("form ra por konid");
     return;
   }
-console.log(window.location);
+  console.log(window.location);
   window.location.replace(
-    window.location.href.replace("frontend/html/index.html","frontend/html/home.html")
+    window.location.href.replace(
+      "frontend/html/index.html",
+      "frontend/html/home.html"
+    )
   );
 });
