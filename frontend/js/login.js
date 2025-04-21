a7fa07a165e245e391edba4a818398b724e3d303
amir-golzar <goliamir1384@gmail.com>
2025-04-21 12:40:06 +0000

    dobare login(submit)

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -2,14 +2,14 @@ const login = document.getElementById("loginform");
 
 login.addEventListener("submit", submit);
 
-const url = "http://127.0.0.1:5500/profile";
-
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
   let expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
+const url = "http://127.0.0.1:5500/sing";
 
 async function submit(e) {
   e.preventDefault();
@@ -19,17 +19,18 @@ async function submit(e) {
 
   const data = { email, password };
 
-  const option = {
+  const response = await fetch(url, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
-  };
-
-  const response = await fetch(url, option);
+  });
 
   const { message, token } = await response.json();
   alert(message);
+  console.log(token);
+
   setCookie("tokenjwt", token, 30);
+
   if ((email === "", password === "")) {
     alert("form ra por konid");
   }
