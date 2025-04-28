c4ee3b874d05c4712447a04410044aa3305f56f6
amir-golzar <goliamir1384@gmail.com>
2025-04-28 12:47:50 +0000

    start put update code forget password

--- backend/back.js
+++ backend/back.js
@@ -28,7 +28,7 @@ const cshema = mongoose.Schema({
 });
 const forgetShema = mongoose.Schema({
   code: { type: "Number", required: true },
-  email: { type: "String", required: true },
+  // email: { type: "String", required: true },
   validDate: { type: "Date", required: true },
 });
 
@@ -47,6 +47,7 @@ const Forget = mongoose.model("forget", forgetShema);
 function forgetCodeFun(e) {
   return Math.floor(100000 + Math.random() * 900000);
 }
+// const fcf=forgetCodeFun()
 
 app.post("/sing", (req, res) => {
   const { name, email, password } = req.body;
@@ -116,39 +117,54 @@ app.post("/EEGP", async (req, res) => {
       .status(404)
       .json({ message: "No account exists, sign up first.", status: 404 });
   } else {
-    // const transporter = nodemailer.createTransport({
-    //   host: process.env.MAIL_HOST,
-    //   port: process.env.MAIL_PORT,
-    //   secure: true,
-    //   auth: {
-    //     user: process.env.MAIL_USER,
-    //     pass: process.env.MAIL_PASSWORD,
-    //   },
-    // });
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+    const theCode = forgetCodeFun();
+    const mailOption = {
+      from: `"login form" <${process.env.MAIL_FROM}>`,
+      to: email,
+      subject: "code forget",
+      test: "your forget password code is",
+      html: `</b> your forget password code is ${theCode} </b>`,
+    };
+    transporter.sendMail(mailOption),
+      (error, info) => {
+        if (error) {
+          return console.log("error " + error.message);
+        }
+        console.log("email sent" + info.respanse);
+      };
+    /*---------------------------------------------- */
+    // const codefarmosh = forgetCodeFun();
+    // console.log(codefarmosh);
 
-    // const mailOption = {
-    //   from: `"login form" <${process.env.MAIL_FROM}>`,
-    //   to: email,
-    //   subject: "code forget",
-    //   test: "your forget password code is",
-    //   html: `</b> your forget password code is ${forgetCodeFun} </b>`,
-    // };
-    // transporter.sendMail(mailOption),
-    //   (error, info) => {
-    //     if (error) {
-    //       return console.log("error " + error.message);
-    //     }
-    //     console.log("email sent" + info.respanse);
-    //   };
-    
+    const vDate = new Date(Date.now() + 30 * 60 * 1000);
+
+    const cemail = new Forget({ email,  validDate: vDate });
+    // const cemail = new Forget({ email, code: theCode, validDate: vDate });
+    cemail.save();
+    /*---------------------------------------------- */
+
     res.status(200).json({
       message: "Enter the recovery code then enter the new password.",
       status: 200,
     });
   }
 });
-app.put("/code", async (req, res) => {
+app.put("/updateCode", async (req, res) => {
   const { code, email, validDate } = req.body;
-  const findEmail = await e;
+  
+  const findforgetD = await Forget.findOne({email});
+  console.log(findforgetD);
+  const validtime=findforgetD.validDate.getTime()
+  console.log(validtime);
+  
 });
 app.listen(process.env.PORT);
